ee8a64168a9ce557e9c6b31d21e07c27dbb580d1
Alex <14583125+alexcaptainhe@user.noreply.gitee.com>
2025-03-07 19:20:38 +0800

    Initial commit

--- app/src/main/java/com/sycreaper/calculator/ui/theme/Theme.kt
+++ app/src/main/java/com/sycreaper/calculator/ui/theme/Theme.kt
@@ -1,42 +1,12 @@
 package com.sycreaper.calculator.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-
-private val DarkColorPalette = darkColorScheme(
-    primary = Purple200,
-    secondary = Teal200,
-    background = Color.DarkGray
-)
-
-private val LightColorPalette = lightColorScheme(
-    primary = Purple500,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
 
 @Composable
 fun CalculatorPrepTheme(
-    darkTheme: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
 
     MaterialTheme(
         typography = Typography,
