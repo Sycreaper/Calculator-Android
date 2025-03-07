23df37e7d2ecc7bd26e4cebece0d27e8cc6f6c47
Alex <14583125+alexcaptainhe@user.noreply.gitee.com>
2025-03-07 19:22:01 +0800

    Initial commit

--- app/src/main/java/com/sycreaper/calculator/ui/theme/Color.kt
+++ app/src/main/java/com/sycreaper/calculator/ui/theme/Color.kt
@@ -2,10 +2,6 @@ package com.sycreaper.calculator.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Teal200 = Color(0xFF03DAC5)
-
 val MediumGray = Color(0xFF2E2E2E)
 val LightGray = Color(0xFF818181)
 val Orange = Color(0xFFFF9800)
