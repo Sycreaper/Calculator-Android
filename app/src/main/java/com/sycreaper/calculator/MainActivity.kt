65bd8f8b3059fd758fc46b9af408ad01f730db31
Alex <14583125+alexcaptainhe@user.noreply.gitee.com>
2025-03-07 19:18:12 +0800

    Initial commit

--- app/src/main/java/com/sycreaper/calculator/MainActivity.kt
+++ app/src/main/java/com/sycreaper/calculator/MainActivity.kt
@@ -5,8 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
